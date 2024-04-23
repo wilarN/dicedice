de6aaa9e17dc8b57dcc73e4e6f5191e8bf12b2ea
William <william.jsson@hotmail.com>
2024-04-23 12:42:10 +0200

    Final

--- public/script/game.js
+++ public/script/game.js
@@ -1,17 +1,23 @@
 
 // William
 
+const pointsToWin = 100;
+
 holdButton = document.getElementById("main-button-hold");
 rollButton = document.getElementById("main-button-roll");
 newGameButton = document.getElementById("main-button-new-game");
 diceImg = document.getElementsByClassName("dice")[0];
+playerWrappers = document.getElementsByClassName("player-wrapper");
+playerTempScores = document.getElementsByClassName("current-score");
+newGameWinButton = document.getElementById("win-button");
+winContainer = document.getElementsByClassName("win-container")[0];
 
-function fetchRandomNumber(tempPrevNum = 0){
+function fetchRandomNumber(tempPrevNum = 0) {
     num = Math.floor(Math.random() * 6) + 1;
-    if(tempPrevNum == 0){
+    if (tempPrevNum == 0) {
         return num;
-    }else{
-        while(num == tempPrevNum){
+    } else {
+        while (num == tempPrevNum) {
             num = Math.floor(Math.random() * 6) + 1;
         }
         return num;
@@ -33,12 +39,12 @@ class player {
     rollDice() {
         console.log("Dice has been rolled!")
         let dice = fetchRandomNumber();
-        
+
         diceImg.style.animation = `spinDice ${this.diceSpeed}s linear`;
         diceImg.src = `img/00dice.png`;
-        
+
         setTimeout(() => {
-            diceImg.src = `img/0${dice}dice.svg`;4
+            diceImg.src = `img/0${dice}dice.svg`; 4
             diceImg.style.animation = "none";
         }, this.diceSpeed * 1000);
 
@@ -53,41 +59,41 @@ class player {
             gameManager.alternateTurn()
             console.log(`Player ${this.currentPlayer} rolled a 1 and has lost all temp points.`);
 
-        if (dice == 2){
-            diceImg.src = "img/02dice.svg";
-            console.log("Rolled 2")
+            if (dice == 2) {
+                diceImg.src = "img/02dice.svg";
+                console.log("Rolled 2")
 
-            this.tempPoints += 2;
-            this.turn = false;
-        }
-        if (dice == 3){
-            diceImg.src = "img/03dice.svg";
-            console.log("Rolled 3")
+                this.tempPoints += 2;
+                this.turn = false;
+            }
+            if (dice == 3) {
+                diceImg.src = "img/03dice.svg";
+                console.log("Rolled 3")
 
-            this.tempPoints += 3;
-            this.turn = false;
-        }
-        if (dice == 4){
-            diceImg.src = "img/04dice.svg";
-            console.log("Rolled 4")
+                this.tempPoints += 3;
+                this.turn = false;
+            }
+            if (dice == 4) {
+                diceImg.src = "img/04dice.svg";
+                console.log("Rolled 4")
 
-            this.tempPoints += 4;
-            this.turn = false;
-        }
-        if (dice == 5){
-            diceImg.src = "img/05dice.svg";
-            console.log("Rolled 5")
+                this.tempPoints += 4;
+                this.turn = false;
+            }
+            if (dice == 5) {
+                diceImg.src = "img/05dice.svg";
+                console.log("Rolled 5")
 
-            this.tempPoints += 5;
-            this.turn = false;
-        }
-        if (dice == 6){
-            diceImg.src = "img/06dice.svg";
-            console.log("Rolled 6")
+                this.tempPoints += 5;
+                this.turn = false;
+            }
+            if (dice == 6) {
+                diceImg.src = "img/06dice.svg";
+                console.log("Rolled 6")
 
-            this.tempPoints += 6;
-            this.turn = false;
-        }
+                this.tempPoints += 6;
+                this.turn = false;
+            }
 
         } else {
             // Continue
@@ -101,17 +107,26 @@ class player {
         return dice;
     }
 
+    // William 23/04/2024
     hold() {
-
-        
         this.points += this.tempPoints;
         console.log(this.playerNum)
         this.tempPoints = 0;
         const totalScore = document.getElementsByClassName(`total0${this.playerNum}`)[0];
+        if(this.points >= pointsToWin){
+            console.log(`Player ${this.playerNum + 1} has won!`);
+            // Display win-container for specific player
+            winContainer.getElementsByClassName("win-text")[0].innerHTML = `Player ${this.playerNum + 1} has won!`;
+            winContainer.style.display = "flex";
+            winContainer.style.animation = "winPopup 1s ease-in-out";
+        }
+
+        for (let i = 0; i < playerTempScores.length; i++) {
+            playerTempScores[i].innerHTML = "Current Points <br> 0";
+        }
 
         totalScore.innerHTML = "Total Points <br>" + this.points;
-        
-        
+
         this.turn = false;
         gameManager.alternateTurn();
     }
@@ -123,25 +138,34 @@ class gameManager {
     constructor(players) {
         this.currentPlayer = 0;
         this.players = players;
+        playerWrappers[0].style.boxShadow = "0px 0px 20px 5px white";
+        playerWrappers[1].style.boxShadow = "none";
     }
 
-    // William
     alternateTurn() {
         if (this.currentPlayer == 0) {
             this.currentPlayer = 1;
+            console.log("Player 2's turn");
+            // Glow on active player
+            playerWrappers[1].style.boxShadow = "0px 0px 10px 5px white";
+            playerWrappers[0].style.boxShadow = "none";
         } else {
             this.currentPlayer = 0;
+            console.log("Player 1's turn");
+            playerWrappers[0].style.boxShadow = "0px 0px 10px 5px white";
+            playerWrappers[1].style.boxShadow = "none";
         }
     }
 
-    newGame(){ 
+    //Simon
+    newGame() {
         //Start new game
         window.location.reload();
-        
+
     }
 }
 
-
+//William
 player01 = new player(0);
 player02 = new player(1);
 players = [player01, player02];
@@ -149,14 +173,19 @@ players = [player01, player02];
 gameManager = new gameManager(players);
 
 
-rollButton.addEventListener("click", function() {
+rollButton.addEventListener("click", function () {
     players[gameManager.currentPlayer].rollDice();
 });
 
-holdButton.addEventListener("click", function() {
+holdButton.addEventListener("click", function () {
     players[gameManager.currentPlayer].hold();
 });
 
-newGameButton.addEventListener("click", function() {
+newGameButton.addEventListener("click", function () {
     gameManager.newGame();
+});
+
+newGameWinButton.addEventListener("click", function () {
+    gameManager.newGame();
+    winContainer.style.display = "none";
 });
